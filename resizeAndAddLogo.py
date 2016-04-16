651c27f8c784c1ed2d6494d9f766aebc42d68d8a
Austin Barnett <abarnett10@mail.valenciacollege.edu>
2016-04-15 20:46:17 -0400

    Created project
    Project creates a new folder for images with logo but images do not
    appear, not sure if this will effect the overall project on your end.

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -12,3 +12,29 @@ logoWidth, logoHeight = logoIm.size
 #TODO: Calculate the new width and height to resize to.
 #TODO: Add the logo.
 #TODO: Save changes.
+
+
+for filename in os.listdir('.'):
+    if not (filename.endswith('.png') or filename.endswith('.jpg')) \
+       or filename == LOGO_FILENAME:
+        continue # skip non-image file and the logo file itself
+
+    im = Image.open(filename)
+    width, height = im.size
+    if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
+        if width > height:
+            height = int((SQUARE_FIT_SIZE / width) * height)
+            width = SQUARE_FIT_SIZE
+        else:
+            width = int((SQUARE_FIT_SIZE / height) * width)
+            height = SQAURE_FIT_SIZE
+
+    print('Resizing %s...' % (filename))
+    im = im.resize((wdith, height))
+
+    print('Adding logo to %s...' % (filename))
+    im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
+
+    im.save(os.path.join('withLogo', filename))
+
+    
